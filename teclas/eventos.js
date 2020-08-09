b4c4b3fae85a1b43deb1bf2272c1d81f6a678cf1
victorhernandez1992 <victorhernandez1992@gmail.com>
2020-08-08 21:50:54 -0600

    day 3 of curse

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,9 +1,35 @@
-document.addEventListener("keydown", dibujarTeclado);
+
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
 
 
-function dibujarTeclado(){
-console.log(
-    "todo bien" 
-)
+ function dibujarTeclado(evento){
+     if(evento.keyCode == teclas.UP){
+        console.log("vamo pa arriba")
+     }
 
+
+switch(evento.keyCode){
+    case teclas.UP:
+        console.log("arriba");
+        break;
+    case teclas.DOWN:
+        console.log("abajo");
+        break;
+
+case teclas.LEFT:
+        console.log("izquierda");
+        break;
+    case teclas.RIGHT:
+        console.log("derecha");
+        break;
+}
 }
