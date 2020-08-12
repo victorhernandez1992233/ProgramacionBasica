fdc13e7bff63e4b65a3e1e925ccb6f57338ba199
victorhernandez1992 <victorhernandez1992@gmail.com>
2020-08-12 17:23:48 -0600

    day 4 of curse

--- villaplatzi/platzi.js
+++ villaplatzi/platzi.js
@@ -2,41 +2,55 @@ var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 var mapa = "tile.png"; 
 
-
-var fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", cargarFondo);
-
-
-
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", cargarVacas);
-
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", cargarCerdos);
-
-
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load", cargarPollos);
-
-
-function dibujar(){
-    papel.drawImage(fondo, 0, 0);
+var fondo = {
+    url: "tile.png",
+    cargaOK: false
 }
 
-function dibujarVacas(){
-    papel.drawImage(vaca, 10, 10);
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false
+};
+
+var cantidad = aleatorio(1, 10);
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+function cargarFondo()
+{
+  fondo.cargaOK = true;
+  dibujar();
+}
+function cargarVacas()
+{
+  vaca.cargaOK = true;
+  dibujar();
 }
 
-function dibujarPollos(){
-    papel.drawImage(pollo, 300, 150);
-}
-
-function dibujarCerdos(){
-    papel.drawImage(cerdo, 10, 300);
+function dibujar()
+{
+  if(fondo.cargaOK)
+  {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if(vaca.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(vaca.imagen, x, y);
+    }
+  }
 }
 // var z;
 // for(var i =0; i<10; i++){
